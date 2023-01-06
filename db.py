078cfcdbdc6e4a1746b8ab9eef0903e46c70b671
Юрий <ygudel@gmail.com>
2023-01-06 16:00:44 +0300

    bd

---
+++ db.py
@@ -0,0 +1,65 @@
+import sqlite3
+
+
+conn = sqlite3.connect('db.sqlite3')
+cur = conn.cursor()
+
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS categories(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(24) NOT NULL,
+    is_published BOOLEAN DEFAULT (true)
+);
+''')
+conn.commit()
+
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS products(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(36) NOT NULL,
+    description VARCHAR(140) NOT NULL,
+    category_id INTEGER NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
+''')
+conn.commit()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS order_items(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL
+);
+''')
+conn.commit()
+
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS orders(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    status_id INTEGER NOT NULL
+);
+''')
+conn.commit()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(24) NOT NULL,
+    email VARCHAR(24) UNIQUE,
+    FOREIGN KEY (user_name) REFERENCES categories(name) ON DELETE CASCADE
+);
+''')
+conn.commit()
+
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS statuses(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(10) UNIQUE
+);
+''')
+conn.commit()
